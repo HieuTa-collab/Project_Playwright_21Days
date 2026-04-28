7ddfaff0ad06c8fa328837783db64fbeb6f8a9c0
hieu <hieu@192.168.20.37.non-exists.ptr.local>
2026-04-28 17:59:17 +0700

    viet herokuapp

---
+++ tests/herokuapp/the-internet-herokuapp.spec.ts
@@ -0,0 +1,84 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach('herokuapp page', async ({ page }) => {
+    //vao page https://the-internet.herokuapp.com/
+    await page.goto('https://the-internet.herokuapp.com/');
+
+    await page.getByText('Welcome to the-internet').isVisible();
+
+});
+
+test('check A/B Testing', async ({ page }) => {
+    //click vào link "A/B Testing"
+    await page.getByRole('link', { name: 'A/B Testing' }).click()
+    
+    await expect(page).toHaveURL(/abtest/);
+});
+
+test('check Add/Remove Elements', async ({ page }) => {
+    //click vào link "Add/Remove Elements"
+await page.getByRole('link', { name: 'Add/Remove Elements' }).click();
+
+    await expect(page).toHaveURL(/add_remove_elements/);
+
+    //Click vào button "Add Element" lần 1
+    await page.getByRole('button', { name: 'Add Element' }).click();
+
+    await expect(page.getByRole('button', { name: 'Delete' })).toBeVisible();
+
+    //Click vào button "Add Element" lần 2
+    await page.getByRole('button', { name: 'Add Element' }).click();
+
+    //Kiểm tra xem có 2 button "Delete" không
+    await expect(page.getByRole('button', { name: 'Delete' })).toHaveCount(2);
+
+
+    });
+
+   //kiểm tra số lượng button "Delete" có phải là 5 không;
+   test ('check Add/Remove Elements - 5 Delete buttons', async ({ page }) => {
+
+    const numOfClicks = 5;
+    const addButton = page.getByRole('button', { name: 'Add Element' });
+    const deleteButtons = page.getByRole('button', { name: 'Delete' });
+
+    await page.getByRole('link', { name: 'Add/Remove Elements' }).click();
+
+for (let i = 0; i < numOfClicks; i++) {
+    await addButton.click();
+    console.log(`Đã click Add Element ${i + 1} lần`);
+    await page.waitForTimeout(200); // Đợi một chút để nút Delete kịp render); 
+  }
+
+    // 4. Quản lý và Verify số lượng hiển thị
+  const actualCount = await deleteButtons.count(); // Đếm số nút thực tế [cite: 656]
+  
+  console.log(`Số lượng nút Delete hiện tại là: ${actualCount}`);
+
+
+//dùng lệnh expect chuyên dụng của Playwright cho locator
+  await expect(deleteButtons).toHaveCount(numOfClicks); //   
+
+ //clicj button "Delete" lần lượt cho đến khi hết 
+ for(let i = 0; i < numOfClicks; i++) {
+    await deleteButtons.first().click(); // Click nút Delete đầu tiên trong danh sách
+    console.log(`Đã click Delete ${i + 1} lần`);
+        await page.waitForTimeout(200); // Đợi một chút để nút Delete kịp render); 
+
+  }
+
+  // Kiểm tra xem có còn nút "Delete" nào không
+  await expect(deleteButtons).toHaveCount(0); // Đảm bảo không còn nút Delete nào hiển thị
+console.log('✅ Đã xóa hết tất cả các nút Delete');    
+});
+
+
+//xử lý checkbox
+test('check Checkboxes', async ({ page }) => {
+    //click vào link "Checkboxes"
+    await page.getByRole('link', { name: 'Checkboxes' }).click();
+
+    await expect(page).toHaveURL(/checkboxes/);
+
+    
+    });
